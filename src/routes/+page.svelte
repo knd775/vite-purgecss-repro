faaf78a752be0322c405694594a0fbd9259f8b4e
Ben Ayles <1235055+knd775@users.noreply.github.com>
2024-04-25 15:31:40 -0400

    improve repro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-5">
-    <h1 class="h1" class:text-red-500={test}>This should be red</h1>
+    <h1 class="h1" class:text-red-500={test}>
+      This should be red, set by a class directive
+    </h1>
+    <h1 class="h1 text-blue-500">This should be blue, but set directly</h1>
   </div>
 </div>
